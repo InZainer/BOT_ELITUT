42bd9463185fd57863fd7c31069f048693351769
Руслан Тинчурин <ruslantincurin@Mac-mini-Ruslan.local>
2025-08-14 15:43:43 +0300

    Refactor, deleted prev ver.

--- handlers/main_helpers.py
+++
@@ -1,9 +0,0 @@
-# handlers/main_helpers.py
-
-from config import ADMINS
-
-# Глобальное хранилище данных пользователей
-user_data = {}
-
-def is_admin(user_id):
-    return user_id in ADMINS
--- utils/validation.py
+++
@@ -1,5 +0,0 @@
-# utils/validation.py
-
-def validate_code(code):
-    # Для примера, принимаем любой 4-значный код
-    return code.isdigit() and len(code) == 4
